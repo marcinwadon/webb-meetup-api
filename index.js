7f038c907d68e6c64cd33c2e43218ba32471934d
Marcin Wadon <marcin@wadon.net>
2016-11-08 11:31:59 +0100

    Enable CORS

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+  next();
+});
 
 // ROUTER
 const router = express.Router();
